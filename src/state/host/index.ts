f8777d79a5c613e2ce04e0a3d81337a93884f574
mikerdean <37405839+mikerdean@users.noreply.github.com>
2023-03-11 21:32:14 +0000

    Add computed fields.

--- src/state/host/index.ts
+++ src/state/host/index.ts
@@ -1,4 +1,4 @@
-import { createRoot, createSignal } from "solid-js";
+import { createMemo, createRoot, createSignal } from "solid-js";
 
 import { Host } from "./types";
 
@@ -9,7 +9,31 @@ export const createHost = () => {
     tcpPort: 9090,
   });
 
-  return { host, setHost };
+  const httpUrl = createMemo((): string | undefined => {
+    const currentHost = host();
+    if (!currentHost) {
+      return undefined;
+    }
+
+    const { hostname, httpPort } = currentHost;
+    const protocol = window.location.protocol;
+    const url = new URL(`${protocol}//${hostname}:${httpPort}`);
+    return url.toString();
+  });
+
+  const websocketUrl = createMemo((): string | undefined => {
+    const currentHost = host();
+    if (!currentHost) {
+      return undefined;
+    }
+
+    const { hostname, tcpPort } = currentHost;
+    const protocol = window.location.protocol === "https:" ? "wss" : "ws";
+    const url = new URL(`${protocol}://${hostname}:${tcpPort}`);
+    return url.toString();
+  });
+
+  return { host, httpUrl, websocketUrl, setHost };
 };
 
 const hostRoot = createRoot(createHost);
