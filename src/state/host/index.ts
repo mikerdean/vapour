56572d7b91d8e2b77741cbc4b299720d2ade9e03
mikerdean <37405839+mikerdean@users.noreply.github.com>
2023-03-11 21:18:32 +0000

    Set the default host value.

--- src/state/host/index.ts
+++ src/state/host/index.ts
@@ -3,15 +3,11 @@ import { createRoot, createSignal } from "solid-js";
 import { Host } from "./types";
 
 export const createHost = () => {
-  /*
-  {
+  const [host, setHost] = createSignal<Host | undefined>({
     hostname: window.location.hostname,
     httpPort: 8080,
     tcpPort: 9090,
-  }
-  */
-
-  const [host, setHost] = createSignal<Host | undefined>(undefined);
+  });
 
   return { host, setHost };
 };
