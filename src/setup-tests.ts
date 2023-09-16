69e7dbc3d23aa8c81bed95aa950154d2d8bb5e7c
mikerdean <37405839+mikerdean@users.noreply.github.com>
2023-09-16 19:56:30 +0100

    Upgrade packages.

--- src/setup-tests.ts
+++ src/setup-tests.ts
@@ -1,13 +1,11 @@
+import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@solidjs/testing-library";
-import matchers from "@testing-library/jest-dom/matchers";
-import { afterEach, expect, vi } from "vitest";
+import { afterEach, vi } from "vitest";
 
 import {
   MockIntersectionObserver,
   resetIntersectionMocking,
 } from "./utils/intersectionObserver";
-
-expect.extend(matchers);
 
 vi.stubGlobal("IntersectionObserver", MockIntersectionObserver);
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ export default defineConfig({
   plugins: [solid()],
   test: {
     css: true,
+    deps: {
+      optimizer: {
+        web: {
+          enabled: false,
+        },
+      },
+    },
     environment: "jsdom",
     globals: false,
     setupFiles: ["./src/setup-tests.ts"],
