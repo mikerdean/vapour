46a7e096943617222b24f66340dfec9bd1062311
mikerdean <37405839+mikerdean@users.noreply.github.com>
2023-11-30 21:15:29 +0000

    Move route definitions to a file I can find.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,28 +1,28 @@
 import type { RouteDefinition } from "@solidjs/router";
 
-import Layout from "../components/layout";
-import Movies from "../components/views/movies";
-import Movie from "../components/views/movies/movie";
-import MovieGenre from "../components/views/movies/movieGenre";
-import MovieGenres from "../components/views/movies/movieGenres";
-import MoviesByTitle from "../components/views/movies/moviesByTitle";
-import MovieSet from "../components/views/movies/movieSet";
-import MovieSets from "../components/views/movies/movieSets";
-import RecentMovies from "../components/views/movies/recentMovies";
-import Music from "../components/views/music";
-import Album from "../components/views/music/album";
-import Albums from "../components/views/music/albums";
-import Artist from "../components/views/music/artist";
-import Artists from "../components/views/music/artists";
-import MusicGenre from "../components/views/music/musicGenre";
-import MusicGenres from "../components/views/music/musicGenres";
-import RecentAlbums from "../components/views/music/recentAlbums";
-import Songs from "../components/views/music/songs";
-import Remote from "../components/views/remote";
-import Settings from "../components/views/settings";
-import TV from "../components/views/tv";
-import TVInProgress from "../components/views/tv/tvInProgress";
-import TVShow from "../components/views/tv/tvShow";
+import Layout from "./components/layout";
+import Movies from "./components/views/movies";
+import Movie from "./components/views/movies/movie";
+import MovieGenre from "./components/views/movies/movieGenre";
+import MovieGenres from "./components/views/movies/movieGenres";
+import MoviesByTitle from "./components/views/movies/moviesByTitle";
+import MovieSet from "./components/views/movies/movieSet";
+import MovieSets from "./components/views/movies/movieSets";
+import RecentMovies from "./components/views/movies/recentMovies";
+import Music from "./components/views/music";
+import Album from "./components/views/music/album";
+import Albums from "./components/views/music/albums";
+import Artist from "./components/views/music/artist";
+import Artists from "./components/views/music/artists";
+import MusicGenre from "./components/views/music/musicGenre";
+import MusicGenres from "./components/views/music/musicGenres";
+import RecentAlbums from "./components/views/music/recentAlbums";
+import Songs from "./components/views/music/songs";
+import Remote from "./components/views/remote";
+import Settings from "./components/views/settings";
+import TV from "./components/views/tv";
+import TVInProgress from "./components/views/tv/tvInProgress";
+import TVShow from "./components/views/tv/tvShow";
 
 const routes: RouteDefinition[] = [
   {
