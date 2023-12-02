219db84ca248aa26b6cb264c539d04f480693e78
mikerdean <37405839+mikerdean@users.noreply.github.com>
2023-12-02 23:10:03 +0000

    Use line clamp for grids.

--- src/components/grid/gridCard.tsx
+++ src/components/grid/gridCard.tsx
@@ -5,7 +5,7 @@ import type { GridCardComponent } from "./gridCard.types";
 const GridCard: GridCardComponent = (props) => {
   return (
     <>
-      <div>{props.title}</div>
+      <div class="line-clamp-2">{props.title}</div>
       <For each={props.items}>
         {(item) => <div class="text-xs text-slate-400 mt-1">{item}</div>}
       </For>
